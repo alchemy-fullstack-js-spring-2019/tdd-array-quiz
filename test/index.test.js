2c43512d3c5bbe09f394213fdd9a57396b704f7a
Sean Nelson <smnelsonn@gmail.com>
2019-04-08 13:18:41 -0700

    setup initial function and test

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,14 @@
-start
+const testFunction = require ('../lib/index.js');
+
+describe('quiz', () => {
+  it('removes words that contain y and Y', () => {
+    const arr = ['sky', 'green', 'Yeti', 'hot'];
+    expect(testFunction(arr)).toEqual(['nope', 'green', 'Yeti', 'hot']);
+  });
+
+  // it('returns -is it $word-?', () ={>{
+
+  // })
+});
+
+//includes
