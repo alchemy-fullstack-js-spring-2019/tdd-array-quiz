64d9c8c696c36210b38e51967adea9c39f94f213
dreamer <wildetangeant@gmail.com>
2019-04-08 13:14:30 -0700

    test infrastructure

---
+++ test/testfunciton.test.js
@@ -0,0 +1,10 @@
+const { filterY } = require('../lib/testFunction.js');
+
+describe('filter function test', () => {
+
+  it('return a new array that filters out the letter y', () => {
+    const sampleData = ['yeti', 'hot', 'sky', 'butterfingers']; 
+
+  });
+    
+});
