64c1fcdbd0e53a5bab9411ad1e7f8088552b2f4c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-08 13:19:02 -0700

    changed 'map' to 'filter' :)

--- test/array.test.js
+++ test/array.test.js
@@ -1,10 +1,10 @@
-const mapArray = require('lib/array');
+const filterArray = require('lib/array');
 
 
 
 describe('takes and array, filters and modifies', () => {
     it('finds items in array containing Y or y', () => {
         const testArray = ['sky', 'hot', 'Yeti', 'green'];
-        expect(mapArray(testArray)).toEqual(['hot', 'green']);
+        expect(filterArray(testArray)).toEqual(['hot', 'green']);
     });
 });
