ec16196998fffdd4bda023956f37742f634931e4
Masmics <cherlocker3113@gmail.com>
2019-04-08 13:43:30 -0700

    push to github

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,15 @@
-const wordArray = require('../lib/wordArray');
+const { wordArray } = require('../lib/index');
 
 
+describe('wordArray function', () => {
+  const array = ['sky', 'hot', 'Yeti', 'green'];
+  // const n1 = 'Y';
+  // const n2 = 'y';
+  it('can remove words containing Y or y', () => {
+    expect(wordArray(array)).toEqual(['Is it hot?', 'Is it green?']);
+
+    // const mock = jest.fn(n => n > 3);
+    // expect(filter([1, 2, 3, 4, 5], mock)).toEqual([4, 5]);
+  });
+
+});
