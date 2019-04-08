3cc687d3cc1be72a94b6f5e7336df63a47a5125b
Laura Badgley <l.badgleyy@gmail.com>
2019-04-08 13:43:16 -0700

    will work on this...

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,13 @@
 
+
 describe('takes an array of strings and creates a new array', () => {
   it('filters out words containing y', () => {
     expect(['sky', 'hot', 'Yeti', 'green']).toEqual(['hot', 'Yeti', 'green']);
   });
+  it('filters out words containing Y', () => {
+    expect(['sky', 'hot', 'Yeti', 'green']).toEqual(['sky', 'hot', 'green']);
+  });
 });
 
-// describe('iterrates over an array', () => {
-//   it('checks to see if words contain the letter y', () => {
-//     expect(['sky', 'hot', 'Yeti', 'green']).toInclude('y');
-//   });
 
-// });
+
