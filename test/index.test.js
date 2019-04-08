2f67e13ffa4e9cbee9b09fb6c1d9d7b03125cdf0
Sean Nelson <smnelsonn@gmail.com>
2019-04-08 14:04:32 -0700

    ..

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ const { correctFunction } = require ('../lib/index.js');
 describe('quiz', () => {
   it('prints back words that are not nope', () => {
     const array2 = ['sky', 'green', 'Yeti', 'hot'];
-    expect(correctFunction(array2)).toEqual(['It is green?', 'It is hot?']);
+    expect(correctFunction(array2)).toEqual(['Is it green?', 'Is it hot?']);
   });
 });
