3ff65328c92bbbe4064cb6e16daff819cb54d9f0
Sean Nelson <smnelsonn@gmail.com>
2019-04-08 13:51:44 -0700

    correct testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,8 @@
 const { testFunction, secondFunction, correctFunction } = require ('../lib/index.js');
 
 describe('quiz', () => {
-  it('removes words that contain y and Y', () => {
-    const arr = ['sky', 'green', 'Yeti', 'hot'];
-    expect(testFunction(arr)).toEqual(['nope', 'green', 'nope', 'hot']);
-  });
-
   it('prints back words that are not nope', () => {
     const array2 = ['sky', 'green', 'Yeti', 'hot'];
-    expect(secondFunction(array2)).toEqual(['It is green', 'It is hot']);
+    expect(correctFunction(array2)).toEqual(['It is green', 'It is hot']);
   });
 });
