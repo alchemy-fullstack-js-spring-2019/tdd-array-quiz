8bf9b7d94cbbf3a48c2b3fad5f08a344f181f1d1
Tommy Tran <tranttommy95@gmail.com>
2019-04-08 13:21:06 -0700

    second test passed

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,19 @@
 const dontAskY = require('../lib/index');
 
 describe('dontAskY', () => {
-  it('Removes any words that contain "y" or "Y" and then makes the question "Is it <word>?" with the remaining words', () => {
+  it('Removes "sky" and "Yeti" and then returns "Is it hot?" and "Is it green?" in an array', () => {
     const expected = ['Is it hot?', 'Is it green?'];
     const input = ['sky', 'hot', 'Yeti', 'green'];
     const result = dontAskY(input);
 
+    expect(result).toEqual(expected);
+  });
+
+  it('Removes "fry" and "WHY" and then returns "Is it cold?" and "Is it coal?" in an array', () => {
+    const expected = ['Is it cold?', 'Is it coal?'];
+    const input = ['cold', 'fry', 'WHY', 'coal'];
+    const result = dontAskY(input);
+  
     expect(result).toEqual(expected);
   });
 });
