5e16ff6de21cb3ce0ca313ec064c61ad5478035e
Ben Parisot <me@benparisot.com>
2019-04-08 13:14:26 -0700

    write and fail initial tests

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+const modifyString = require('../lib/index');
+
+describe('string modifier', () => {
+  it('finds any instance of y or Y and removes that string from the array', () => {
+    const input = ['sky', 'hot', 'Yeti', 'green'];
+    const expected = ['hot', 'green'];
+
+    expect(modifyString(input)).toEqual(expected);
+
+  });
+
+  it('takes the remaining words in the array and puts them into a new string', () => {
+    const input = ['sky', 'hot', 'Yeti', 'green'];
+    const expected = ['Is it hot?', 'Is it green?'];
+
+    expect(modifyString(input)).toEqual(expected);
+  });
+});
