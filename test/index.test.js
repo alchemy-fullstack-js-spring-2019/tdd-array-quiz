09c8e22a1962e1ee60e41dc73a5b0357d8949717
Cheri Portman <cheriportman@hotmail.com>
2019-04-08 13:44:03 -0700

    turn in

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
 const { noY } = require('../lib/index.js');
 
 describe('function filters out words with the Y and changes message with remaining', () => {
-  it('filters out words that contain y or Y', () => {
+  it('return words that contain y', () => {
     const words = ['sky', 'hot', 'Yeti', 'green'];
 
-    expect(noY(words)).toEqual(['hot', 'green']);
+    expect(noY(words)).toEqual(['sky']);
   });
 });
