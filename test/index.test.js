6b0dd60fe81133d378644d549b96b368026b9ddb
Cara Ponzini <ponzini@gmail.com>
2019-04-08 13:41:48 -0700

    wrote separate test for  sentence return and pass

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,20 @@
 const { why } = require('../lib/index');
+const { isIt } = require('../lib/index');
 
 describe('don\'t ask why', () => {
   test('it returns an array of words that include y', () => {
     const array = ['sky', 'hot', 'Yeti', 'green'];
     const result = why(array);
-    expect(result).toEqual(['hot', 'green']);
+    expect(result).toEqual(['sky', 'Yeti']);
   });
 
-//   test('it removes words that include y', () => {
-//     const array = ['sky', 'hot', 'Yeti', 'green'];
-//     const result = why(array);
-//     expect(result).toEqual(['sky', 'Yeti']);
-//   });
+  test('it returns a string with the y word in it', () => {
+    const array = ['sky', 'hot', 'Yeti', 'green'];
+    const returnArray = ['Is it sky?', 'Is it Yeti?'];
+    const result = isIt(array);
+    expect(result).toEqual(returnArray);
+  });
+
 
 });
 
