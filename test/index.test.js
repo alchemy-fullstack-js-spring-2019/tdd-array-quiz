0dde73fc3f7610820f80fabb5ccb50e5ceb3c9ca
Leslie Shea <leslieyshea@gmail.com>
2019-04-08 13:42:08 -0700

    wrote second test

--- test/index.test.js
+++ test/index.test.js
@@ -6,4 +6,10 @@ describe('transformArray function', () => {
     const result = transformArray(words);
     expect(result).toEqual(['sky', 'hot', 'green']);
   });
+
+  it('changes new array with new sentence', () => {
+    const words = ['sky', 'hot', 'Yeti', 'green'];
+    const result = transformArray(words);
+    expect(result).toEqual(['Is it sky?', 'Is it hot?', 'Is it green?']);
+  });
 });
