5fd5f191e8cc9354cde9cbbc3161f55d1effb2b8
Payton Lower <paytown@me.com>
2019-04-08 13:19:43 -0700

    passing when hard coded. jest working

--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,8 @@ const library = require('../lib/index');
 describe('library function', () => {
 
   it('take array of words and return words that remove Y or y', () => {
-    const array = ['sky', 'hot', 'Yeti', 'green'];    
-    expect(library(array)).toEqual([['hot', 'green']]);
+    const arr = ['sky', 'hot', 'Yeti', 'green'];    
+    expect(library(arr)).toEqual(['hot', 'green']);
   });
 
 });
