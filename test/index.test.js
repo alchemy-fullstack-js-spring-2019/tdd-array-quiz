866a26d14cbed316be2fec73d4ed614350b96302
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-08 13:27:12 -0700

    second test failing (adding words to string

--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,10 @@ const yFilter = require('../lib/index.js');
 describe('yFilter function test', () => {
   it('removes all words that contain the letter y or Y', () => {
     const yWords = ['sky', 'Yeti', 'myth'];
-
-
     expect(yFilter(yWords)).toEqual([]);
+  });
+  it('adds `Is it + ?` to strings that don`t contain a y', () => {
+    const input = ['sky', 'hot', 'Yeti', 'green'];
+    expect(yFilter(input)).toEqual(['Is it hot?', 'Is it green?']);
   });
 });
