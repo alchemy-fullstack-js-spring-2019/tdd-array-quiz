b1b39f3e5d4d6acbfd355289f182576e0b354442
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-08 13:38:23 -0700

    passing with lowercase y

--- test/stringies.test.js
+++ test/stringies.test.js
@@ -2,7 +2,7 @@ const { stringies } = require('../lib/stringies.js');
 
 describe('using map and filter to return a string', () => {
   it('takes a function with array param and return string with words that dont contain y', () => {
-    const fakeArr = ['yellow', 'pair', 'Yellow', 'blue'];
-    expect(stringies(fakeArr)).toBe(['You are so pair', 'You are so blue']);
+    const fakeArr = ['yellow', 'pair', 'blue'];
+    expect(stringies(fakeArr)).toEqual(['You are so pair', 'You are so blue']);
   });
 });
