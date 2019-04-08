8bb27403e4b9fc02450fbcbb45f5cf0895214ac4
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-08 13:19:04 -0700

    Wrote failing test for filter

--- test/index.test.js
+++ test/index.test.js
@@ -7,4 +7,11 @@ describe('Don\'t ask why', () => {
     const result = removeAndMod(input);
     expect(result).toEqual(expected);
   });
+  
+  it('Modify the original word to be a phrase and removes words containing Y or y', () => {
+    const expected = ['Is it hot', 'Is it green'];
+    const input = ['hot', 'yeti', 'sky', 'green'];
+    const result = removeAndMod(input);
+    expect(result).toEqual(expected);
+  });
 });
