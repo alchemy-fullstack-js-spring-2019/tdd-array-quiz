c7482d195588afaad66a17f894e980045d66cff6
Cheri Portman <cheriportman@hotmail.com>
2019-04-08 13:18:18 -0700

    dependencies installed, inital test started, not done

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,12 @@
+const { noY } = require('../lib/index.js');
 
+describe('', () {
+  it('', () {
+    const words = ['sky', 'hot', 'Yeti', 'green'];
+
+  });
+});
+
+
+//goals: remove any words that contain Y or y
+//word changed to 'Is it <word>
