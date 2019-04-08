14fd932487f8907c0c455b7838c4bf90a84685c7
Cheri Portman <cheriportman@hotmail.com>
2019-04-08 13:22:14 -0700

    first test written

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,16 @@
 const { noY } = require('../lib/index.js');
 
-describe('', () {
-  it('', () {
+describe('function filters out words with the Y and changes message with remaining', () {
+  it('filters out words that contain y', () {
     const words = ['sky', 'hot', 'Yeti', 'green'];
 
+    expect(noY(words)).toEqual(['hot', 'green'])
   });
 });
 
 
 //goals: remove any words that contain Y or y
-//word changed to 'Is it <word>
+//word changed to 'Is it <word>n
+
+
+//what do I start with... first test 
