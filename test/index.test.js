c5ea6f56644a54fa1efe68388676b59af59bfe51
mcnadams <mcneilbo@oregonstate.edu>
2019-04-08 13:24:00 -0700

    add test cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,22 @@
 const dontAskWhy = require('../lib/index');
 
 describe('don\'t ask why', () => {
-  const words = ['sunny', 'raining', 'cold', 'windy'];
   it('takes an array of words and modifies it', () => {
+    const words = ['sunny', 'raining', 'cold', 'windy'];
     const result = dontAskWhy(words);
     const expected = ['Is it raining?', 'Is it cold?'];
+    expect(result).toEqual(expected);
+  });
+  it('takes another array of words which all contain y and returns an empty array', () => {
+    const words = ['funny', 'HAPPY', 'silly'];
+    const result = dontAskWhy(words);
+    const expected = [];
+    expect(result).toEqual(expected);
+  });
+  it('takes an array of strings and modifies it', () => {
+    const words = ['tired', 'grumpy', 'UPSET WITH ME', 'GOING TO LEAVE', 'UGLY'];
+    const result = dontAskWhy(words);
+    const expected = ['Is it tired?', 'Is it UPSET WITH ME?', 'Is it GOING TO LEAVE?'];
     expect(result).toEqual(expected);
   });
 });
