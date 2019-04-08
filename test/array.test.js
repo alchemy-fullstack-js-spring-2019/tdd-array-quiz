4251111495b6d0b4886929160c3b309b678deb01
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-08 13:17:06 -0700

    wrote map test

--- test/array.test.js
+++ test/array.test.js
@@ -1,5 +1,10 @@
+const mapArray = require('lib/array');
+
 
 
 describe('takes and array, filters and modifies', () => {
-    it('finds items in array containing Y or y', () => )
-})
+    it('finds items in array containing Y or y', () => {
+        const testArray = ['sky', 'hot', 'Yeti', 'green'];
+        expect(mapArray(testArray)).toEqual(['hot', 'green']);
+    });
+});
