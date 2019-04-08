5542cb4884afd44f0d06b63d2ac468591959d889
Laura Badgley <l.badgleyy@gmail.com>
2019-04-08 13:37:38 -0700

    tried a couple different ways but could not get tests to pass... haven't gotten to modifying the word

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,13 @@
+
 describe('takes an array of strings and creates a new array', () => {
   it('filters out words containing y', () => {
-
+    expect(['sky', 'hot', 'Yeti', 'green']).toEqual(['hot', 'Yeti', 'green']);
   });
 });
+
+// describe('iterrates over an array', () => {
+//   it('checks to see if words contain the letter y', () => {
+//     expect(['sky', 'hot', 'Yeti', 'green']).toInclude('y');
+//   });
+
+// });
