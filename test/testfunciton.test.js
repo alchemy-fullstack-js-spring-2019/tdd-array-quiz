02faa1ac29382ac9e3e5ef3417b3024c448160e4
dreamer <wildetangeant@gmail.com>
2019-04-08 13:27:34 -0700

    dependenceies installed, failling test written, writing function

--- test/testfunciton.test.js
+++ test/testfunciton.test.js
@@ -3,7 +3,10 @@ const { filterY } = require('../lib/testFunction.js');
 describe('filter function test', () => {
 
   it('return a new array that filters out the letter y', () => {
-    const sampleData = ['yeti', 'hot', 'sky', 'butterfingers']; 
+    const result = filterY(['yeti', 'hot', 'sky', 'butterfingers']); 
+
+    expect(result).toEqual(['hot', 'butterfingers']);
+
 
   });
     
