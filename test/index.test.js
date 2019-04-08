bd416b40db1f47c329c9514121bf5c7a1d52ba05
Sean Nelson <smnelsonn@gmail.com>
2019-04-08 13:56:11 -0700

    reviewing where I went wrong, and how to make it so simple

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
-const { testFunction, secondFunction, correctFunction } = require ('../lib/index.js');
+const { correctFunction } = require ('../lib/index.js');
 
 describe('quiz', () => {
   it('prints back words that are not nope', () => {
     const array2 = ['sky', 'green', 'Yeti', 'hot'];
-    expect(correctFunction(array2)).toEqual(['It is green', 'It is hot']);
+    expect(correctFunction(array2)).toEqual(['It is green?', 'It is hot?']);
   });
 });
