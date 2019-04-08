a7667e694ee7253fb8843e711d5d181460935f93
mcnadams <mcneilbo@oregonstate.edu>
2019-04-08 13:15:57 -0700

    make failing test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,10 @@
 const dontAskWhy = require('../lib/index');
 
 describe('don\'t ask why', () => {
+  const words = ['sunny', 'raining', 'cold', 'windy'];
   it('takes an array of words and modifies it', () => {
-        
+    const result = dontAskWhy(words);
+    const expected = ['Is it raining?', 'Is it cold?'];
+    expect(result).toEqual(expected);
   });
 });
