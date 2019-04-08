0481551a3dc05af17617c81f217ba84a720bd86e
Peter Kotowski <bighoopee@gmail.com>
2019-04-08 13:38:42 -0700

    wrote a mock test

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+
+
+
+describe('function that creates a new array and returns an array that removes word beginning with y', () => {
+  it('can create a new array', () => {
+    const arr = ['yellow', 'blue', 'green']
+    const newArr = arr.map();
+    return newArr;
+  });
+});
