b2b35e5eb1a002b3bf9ea9722e93b4e2bfd8947f
Cheri Portman <cheriportman@hotmail.com>
2019-04-08 13:35:21 -0700

    can isolate sky from array

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,10 @@
 const { noY } = require('../lib/index.js');
 
-describe('function filters out words with the Y and changes message with remaining', () {
-  it('filters out words that contain y', () {
+describe('function filters out words with the Y and changes message with remaining', () => {
+  it('filters out words that contain y or Y', () => {
     const words = ['sky', 'hot', 'Yeti', 'green'];
 
-    expect(noY(words)).toEqual(['hot', 'green'])
+    expect(noY(words)).toEqual(['hot', 'green']);
   });
 });
 
