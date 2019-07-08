604fe00aeeb24ad84e2b37f3219f72b28d98e3b9
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-08 13:51:08 -0700

    init

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+
+
+describe('tdd test', () => {
+  it('that takes an array of words and returns a new array with only words containing no "y"', () => {
+    const arr = ['sky', 'hot', 'Yeti', 'green'];
+    const newArr = [];
+    expect()
+  });
+});
