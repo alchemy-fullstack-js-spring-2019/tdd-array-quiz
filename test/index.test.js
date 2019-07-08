2aade0ac63d61ff1ea9254fc3a8a4d370d79af5e
Carlos Us Santiago <cdus12@gmail.com>
2019-07-08 14:16:37 -0700

    continued working on test

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@ const { filter } = require('../test/index.test');
 describe('filter an array', () => {
   it('filter any words than contain the letter Y or y', () => {
     const words = ['sky', 'hot', 'Yeti', 'green'];
+    const why = filter(words, word => word != )
     let newArr = [];
 
     const filtered = ();
