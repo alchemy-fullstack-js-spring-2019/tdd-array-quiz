7ffc4b171b22ae2ecce04486b8671a9572d9aa12
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-08 14:17:01 -0700

    I wasn't able to solve the problem, or get any of my smaller steps to pass the test, I kept ammending code, so please look at my commit history to at least get my thought pattern

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,10 @@
 const { dontAskWhy } = require('../lib/index.js');
 
 describe('dontAskWhy function returns altered array', () => {
-    // it('dontAskWhy removes any strings with y', () => {
-    //     const doesInclude = dontAskWhy(['why', 'is', 'this']);
-    //     expect(doesInclude).toBeTruthy();
-    // });
+    it('dontAskWhy removes any strings with y', () => {
+        const doesInclude = dontAskWhy(['why', 'is', 'this']);
+        expect(doesInclude).toBeTruthy();
+    });
 
     it('dontAskWhy adds "is it?" in front of other words,', () => {
      const addsWords = dontAskWhy(['testing', 'working']);
