e6f27594d3bd587208546ad4693845cb3780316d
DannySuarez <suarezd10@gmail.com>
2019-07-08 14:13:36 -0700

    started writing out function

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,9 @@
+const { dontAskWhy } = require('../lib/index');
 
 describe('TDD Testing', () => {
   it('removes words and returns a new array', () => {
-    
+    const initArray = ['sky', 'hot', 'Yeti', 'green'];
+    const newArray = ['Is it hot?', 'Is it green?'];
+
   });
 });
