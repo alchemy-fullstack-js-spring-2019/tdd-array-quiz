5c8fb239c56918df693a8977dea619f4a8f04992
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-07-08 14:05:41 -0700

    edited readme

--- test/why.test.js
+++ test/why.test.js
@@ -3,7 +3,7 @@ const { why } = require('../lib/why');
 describe('oh man what is happening to this class I am gonna cry', () => {
 
 
-    it('if given a string of words, removes the WORD that contains the letter Y', () => {
+    it('if given a string of words, removes the WORD that contains the letter Y and returns correctly formatted sentence', () => {
 
         const input = ['why', 'how', 'what', 'yes'];
 
