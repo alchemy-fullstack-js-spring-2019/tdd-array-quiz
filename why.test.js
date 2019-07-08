4f7579fbc65513339e06eb9048b64ad6e4789c2d
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-07-08 13:51:44 -0700

    added import and exports, more testing structure

--- why.test.js
+++ why.test.js
@@ -3,7 +3,15 @@ const { why } = require('./why');
 describe('oh man what is happening to this class I am gonna cry', () => {
 
 
-    it('does something', () => {
+    it('if given a string of words, removes the WORD that contains the letter Y', () => {
+
+        const input = 'why so serious';
+
+        const expected = 'is it so?';
+
+        const result = why(input);
+
+        expect(result).toBe(expected);
 
     }); 
 
