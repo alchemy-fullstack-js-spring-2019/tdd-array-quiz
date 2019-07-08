eba8cb19d3e108b36d09d5b8f5e31ec14f1ed0ee
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-07-08 13:56:53 -0700

    fixed test to take array instead of string

--- test/why.test.js
+++ test/why.test.js
@@ -5,9 +5,9 @@ describe('oh man what is happening to this class I am gonna cry', () => {
 
     it('if given a string of words, removes the WORD that contains the letter Y', () => {
 
-        const input = 'why so serious';
+        const input = ['why', 'how', 'what', 'yes'];
 
-        const expected = 'is it so?';
+        const expected = ['is it how?', 'is it what?'];
 
         const result = why(input);
 
