239d6f8649199cccd3435f01a2654b5380f55dd0
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-08 14:10:24 -0700

    built functions and tested

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,15 @@
-
+const { arrFilter, arrFilterAddOn } = require('../lib/index');
 
 describe('tdd test', () => {
   it('that takes an array of words and returns a new array with only words containing no "y"', () => {
     const arr = ['sky', 'hot', 'Yeti', 'green'];
-    const newArr = [];
-    expect()
+    const newArr = arrFilter(arr);
+    expect(newArr).toEqual(['hot', 'green']);
+  });
+
+  it('that takes the new array and adds "Is it..."', () => {
+    const arr = ['sky', 'hot', 'Yeti', 'green'];
+    const newArr = arrFilterAddOn(arr);
+    expect(newArr).toEqual(['Is it hot?', 'Is it green?']);
   });
 });
