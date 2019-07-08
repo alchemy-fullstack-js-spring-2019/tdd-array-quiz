e3de780f011d47401f87f077229417a9ee10367c
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-08 14:08:28 -0700

    fixed function import and made test to remove everything with y and that passes

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,15 @@
-const removeYAddText = require('../lib/index');
+const { removeYAddText } = require('../lib/index');
 
 describe('testing the removeYAddText function', () => {
     it('returns an array', () => {
         const someArray = ['sky', 'hot', 'Yeti', 'green'];
         const result = removeYAddText(someArray);
         expect(result).toEqual(expect.any(Array));
+    });
+
+    it('returns an array without words containing leter y', () => {
+        const someArray = ['sky', 'hot', 'Yeti', 'green'];
+        const result = removeYAddText(someArray);
+        expect(result).toEqual(['hot', 'green']);
     })
 })
