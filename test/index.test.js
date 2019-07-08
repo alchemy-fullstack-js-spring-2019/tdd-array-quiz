ab524479c572ad12fd6792400235f2cac4918bac
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-08 14:13:45 -0700

    tried attacking the second part of the problem still with no success, just committing so you can see my code

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,13 @@
 const { dontAskWhy } = require('../lib/index.js');
 
 describe('dontAskWhy function returns altered array', () => {
-    it('dontAskWhy removes any strings with y', () => {
-        const doesInclude = dontAskWhy(['why', 'is', 'this']);
-        expect(doesInclude).toBeTruthy();
-    });
+    // it('dontAskWhy removes any strings with y', () => {
+    //     const doesInclude = dontAskWhy(['why', 'is', 'this']);
+    //     expect(doesInclude).toBeTruthy();
+    // });
 
-    it('dontAskWhy removes any strings with y AND adds "is it?" in front of other words,', () => {
-    
+    it('dontAskWhy adds "is it?" in front of other words,', () => {
+     const addsWords = dontAskWhy(['testing', 'working']);
+     expect(addsWords).toEqual(['is it testing?', 'is it working?'])
     });
 });
