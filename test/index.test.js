5c9b8ce9758e881da19e7a6de8540a9cc230a778
Jonathan Forney <jonathan.forney@gmail.com>
2019-07-08 14:14:44 -0700

    as far as i got

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,28 @@
+const ourFunc = require('../lib/index');
+
 const ourArr = ['sky', 'hot', 'Yeti', 'green'];
 
 describe('test function returns no y words and is it', () => {
   it('removes words with y', () => {
 
+    ourFunc(ourArr);
+
     const expectedRes = ['hot', 'green'];
     
+    expect(ourArr).toEqual(expectedRes);
+  });
+
+  it('adds is it to each word', () => {
+
+    function part2(arr) {
+      arr.forEach(element => {
+        element = 'Is it ' + element;
+      });
+      return arr;
+    }
+
+    const expectedRes = ['Is it hot', 'Is it green'];
+
     expect(ourArr).toEqual(expectedRes);
   });
 
