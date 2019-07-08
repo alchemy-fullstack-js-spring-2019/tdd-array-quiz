246a0d2c0fbd529b76979903709907d8a99f7b5b
Claire Cassidy <clacassidy@gmail.com>
2019-07-08 14:08:06 -0700

    attempted function and test

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,9 @@
-const { }
+const { isItWithNoY } = require('../lib/index');
+
+describe('Makes an is it function with no Ys upper or lowercase', () => {
+  it('checks the array for Ys both upper and lowercase', () => {
+    const array = ['sky', 'hot', 'Yeti', 'green']	
+
+    expect()
+  });
+};
